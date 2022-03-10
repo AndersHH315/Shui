0aafecf4fa814c837b840574d975327119d28cb0
Anders Hansen-Haug <andershh_93@hotmail.com>
2022-03-10 14:39:51 +0100

    fix

--- server/Db/connection.js
+++ server/Db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://Anders:asd123@myfirstcluster.pmnkh.mongodb.net/shuidb?retryWrites=true&w=majority';
+const url = 'mongodb+srv://Anders:######@myfirstcluster.pmnkh.mongodb.net/shuidb?retryWrites=true&w=majority';
 
 async function dbConnect() {
     mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
